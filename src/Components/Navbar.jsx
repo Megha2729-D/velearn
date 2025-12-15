85464d3c3743e9870eba75254e8eb4172f8d8da4
Megha2729-D <134476584+Megha2729-D@users.noreply.github.com>
2025-12-15 19:50:52 +0530

    alignment fixed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,6 +4,8 @@ import { Link, NavLink } from 'react-router-dom';
 const Navbar = () => {
     const [showNavbar, setShowNavbar] = useState(false);
     const [active, setActive] = useState(false);
+    const [practiceOpen, setPracticeOpen] = useState(false);
+    const [resourcesOpen, setResourcesOpen] = useState(false);
 
     const handleShowNavbar = () => {
         setShowNavbar(!showNavbar);
@@ -11,11 +13,7 @@ const Navbar = () => {
 
     useEffect(() => {
         const handleScroll = () => {
-            if (window.scrollY > 100) {
-                setActive(true);
-            } else {
-                setActive(false);
-            }
+            setActive(window.scrollY > 100);
         };
 
         window.addEventListener("scroll", handleScroll);
@@ -29,87 +27,103 @@ const Navbar = () => {
         } else {
             document.body.style.overflow = '';
             document.documentElement.style.overflow = '';
+            setPracticeOpen(false);
+            setResourcesOpen(false);
         }
 
-        // Cleanup just in case
         return () => {
             document.body.style.overflow = '';
             document.documentElement.style.overflow = '';
         };
     }, [showNavbar]);
+
     return (
-        <nav className="v-navbar flex-column w-100">
-            <div className={`navbar_links ${active ? "active" : ""}`}>
-                <div className="container-fluid p-xl">
-                    <div className="nav_parent py-3">
-                        <div className="menu-icon" onClick={handleShowNavbar}>
-                            <Hamburger isOpen={showNavbar} />
-                        </div>
-                        <div className="logo">
-                            <NavLink to="/">
-                                <img src="assets/images/velearn-logo.png" alt="" />
-                            </NavLink>
-                        </div>
-                        <div className={`nav-elements  ${showNavbar && "active"}`}>
-                            <ul className="mb-0">
-                                <li>
-                                    <NavLink to="/">Self-Paced Course</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/sample">Live Course</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/sample">Practice</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/sample">Resources</NavLink>
-                                </li>
-                            </ul>
-                            <div className="col-lg-12 d-flex gap-2 d-lg-none px-4 my-3">
-                                <a href="/">
-                                    <div className="ab-lgo-bx">
-                                        <i className="bi bi-instagram"></i>
-                                    </div>
-                                </a>
-                                <a href="/">
-                                    <div className="ab-lgo-bx">
-                                        <i className="bi bi-facebook"></i>
-                                    </div>
-                                </a>
-                                <a href="/">
-                                    <div className="ab-lgo-bx">
-                                        <i className="bi bi-twitter-x"></i>
-                                    </div>
-                                </a>
-                                <a href="/">
-                                    <div className="ab-lgo-bx">
-                                        <i className="bi bi-whatsapp"></i>
-                                    </div>
-                                </a>
+        <nav className="v-navbar flex-column w-100 bg-white">
+            <div className={`navbar_links section_container ${active ? "active" : ""}`}>
+                <div className="nav_parent py-1">
+
+                    {/* Hamburger */}
+                    <div className="menu-icon" onClick={handleShowNavbar}>
+                        <Hamburger isOpen={showNavbar} />
+                    </div>
+
+                    {/* Logo */}
+                    <div className="logo">
+                        <NavLink to="/">
+                            <img
+                                src={`${process.env.PUBLIC_URL}/assets/images/velearn-logo.png`}
+                                alt="Velearn"
+                            />
+                        </NavLink>
+                    </div>
+
+                    {/* Nav Links */}
+                    <div className={`nav-elements ${showNavbar ? "active" : ""}`}>
+                        <ul className="mb-0 p-0">
+
+                            <li>
+                                <NavLink to="/">Self-paced Courses</NavLink>
+                            </li>
+
+                            <li>
+                                <NavLink to="/sample">Live Courses</NavLink>
+                            </li>
+
+                            {/* Practice Dropdown */}
+                            <li
+                                className={`dropdown ${practiceOpen ? "open" : ""}`}
+                                onClick={() => setPracticeOpen(!practiceOpen)}
+                            >
+                                <span className="dropdown-toggle">
+                                    Practice <i className="bi bi-chevron-down"></i>
+                                </span>
+
+                                <ul className="dropdown-menu">
+                                    <li><NavLink to="/practice/ide">Online IDE</NavLink></li>
+                                    <li><NavLink to="/practice/debugging">Debugging</NavLink></li>
+                                    <li><NavLink to="/practice/challenges">Challenges</NavLink></li>
+                                </ul>
+                            </li>
+
+                            {/* Resources Dropdown */}
+                            <li
+                                className={`dropdown ${resourcesOpen ? "open" : ""}`}
+                                onClick={() => setResourcesOpen(!resourcesOpen)}
+                            >
+                                <span className="dropdown-toggle">
+                                    Resources <i className="bi bi-chevron-down"></i>
+                                </span>
+
+                                <ul className="dropdown-menu">
+                                    <li><NavLink to="/resources/blogs">Blogs</NavLink></li>
+                                    <li><NavLink to="/resources/docs">Docs</NavLink></li>
+                                    <li><NavLink to="/resources/tools">Tools</NavLink></li>
+                                </ul>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                    {/* Right Section */}
+                    <div className="d-flex gap-4 right_nav_icons">
+
+                        <div className="d-flex align-items-center">
+                            <div className="d-flex align-items-center search_parent position-relative">
+                                <i className="bi bi-search"></i>
+                                <input type="search" placeholder="Search" />
                             </div>
                         </div>
-                        <div className="d-flex gap-4 right_nav_icons">
-                            <div className="d-flex gap-4 text-white">
-                                <span className='d-none d-lg-block'>
-                                    <Link to='/login' className="text-c1 text-decoration-none fw-bold">Login</Link>
-                                </span>
-                                <span>
-                                    <Link to="/signup" className="text-decoration-none signUpBut px-4">Sign Up</Link>
-                                </span>
-                            </div>
+
+                        <div className="d-flex gap-2 text-white">
+                            <span className="d-none d-lg-block">
+                                <Link to="/login" className="btn_login">Login</Link>
+                            </span>
+                            <span className="d-none d-lg-block">|</span>
+                            <span>
+                                <Link to="/signup" className="btn_signup">Sign Up</Link>
+                            </span>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div className="container-fluid p-xl search_parent d-lg-block d-none">
-                <div className="col-12 d-flex justify-content-end align-items-center gap-4 py-2">
-                    <div className="w-100 d-flex justify-content-end">
-                        <input type="search" style={{ width: "40%" }} placeholder='Search....' />
-                    </div>
-                    <div className="d-flex gap-2 ">
-                        <span>
-                            <Link to="/" className="text-decoration-none signUpBut px-4">Contact</Link>
-                        </span>
+
                     </div>
                 </div>
             </div>
@@ -128,38 +142,14 @@ const Hamburger = ({ isOpen }) => {
                 viewBox="0 0 52 24"
                 className={isOpen ? "d-none" : "d-block"}
             >
-                <g id="Group_9" data-name="Group 9" transform="translate(-294 -47)">
-                    <rect
-                        id="Rectangle_3"
-                        data-name="Rectangle 3"
-                        width="30"
-                        height="2"
-                        rx="2"
-                        transform="translate(304 47)"
-                        fill="#574c4c"
-                    />
-                    <rect
-                        id="Rectangle_5"
-                        data-name="Rectangle 5"
-                        width="30"
-                        height="2"
-                        rx="2"
-                        transform="translate(304 67)"
-                        fill="#574c4c"
-                    />
-                    <rect
-                        id="Rectangle_4"
-                        data-name="Rectangle 4"
-                        width="40"
-                        height="2"
-                        rx="2"
-                        transform="translate(294 57)"
-                        fill="#574c4c"
-                    />
+                <g transform="translate(-294 -47)">
+                    <rect width="30" height="2" rx="2" transform="translate(304 47)" fill="#574c4c" />
+                    <rect width="40" height="2" rx="2" transform="translate(294 57)" fill="#574c4c" />
+                    <rect width="30" height="2" rx="2" transform="translate(304 67)" fill="#574c4c" />
                 </g>
             </svg>
 
-            {/* X icon */}
+            {/* Close icon */}
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -173,6 +163,5 @@ const Hamburger = ({ isOpen }) => {
         </>
     );
 };
-
 
 export default Navbar;

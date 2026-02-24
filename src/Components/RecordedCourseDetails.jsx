49817c2e8d4328f673aebeb6d4b72020dbb09474
Megha <meghu27022002@gmail.com>
2026-02-24 13:46:33 +0530

    Merged remote repository

--- src/Components/RecordedCourseDetails.jsx
+++ src/Components/RecordedCourseDetails.jsx
@@ -708,7 +708,7 @@ class RecordedCourseDetails extends Component {
                                                 </div>
 
                                                 <div className="modal-body">
-                                                    <div className="tabs-wrapper" ref={this.comboTabsWrapperRef} style={{ position: "relative", minHeight: "300px" }}>
+                                                    {/* <div className="tabs-wrapper" ref={this.comboTabsWrapperRef} style={{ position: "relative", minHeight: "300px" }}>
                                                         <div
                                                             className="tabs"
                                                             ref={this.comboTabsRef}
@@ -772,6 +772,43 @@ class RecordedCourseDetails extends Component {
                                                                 ))}
                                                             </ul>
                                                         </div>
+                                                    </div> */}
+                                                    <div className="accordion" id="comboAccordion">
+                                                        {this.state.selectedComboModules?.map((module, index) => (
+                                                            <div className="accordion-item" key={module.id}>
+
+                                                                <h2 className="accordion-header" id={`heading${index}`}>
+                                                                    <button
+                                                                        className={`accordion-button ${index !== 0 ? "collapsed" : ""}`}
+                                                                        type="button"
+                                                                        data-bs-toggle="collapse"
+                                                                        data-bs-target={`#collapse${index}`}
+                                                                        aria-expanded={index === 0 ? "true" : "false"}
+                                                                        aria-controls={`collapse${index}`}
+                                                                    >
+                                                                        {module.title}
+                                                                        {/* Module {index + 1} */}
+                                                                    </button>
+                                                                </h2>
+
+                                                                <div
+                                                                    id={`collapse${index}`}
+                                                                    className={`accordion-collapse collapse ${index === 0 ? "show" : ""}`}
+                                                                    aria-labelledby={`heading${index}`}
+                                                                    data-bs-parent="#comboAccordion"
+                                                                >
+                                                                    <div className="accordion-body">
+                                                                        {/* <h6 className="mb-3">{module.title}</h6> */}
+                                                                        <ul>
+                                                                            {module.points?.map((point, i) => (
+                                                                                <li key={i}>{point}</li>
+                                                                            ))}
+                                                                        </ul>
+                                                                    </div>
+                                                                </div>
+
+                                                            </div>
+                                                        ))}
                                                     </div>
                                                 </div>
 
